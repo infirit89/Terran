39d86af00f869989c8f027fd9414f8ff59e4e0ed
Jordan-Dimitrov <dani27game@gmail.com>
2024-04-11 09:55:42 +0300

    added mouse scene

--- TerranEditor/SandboxProject/Assets/Scripts/Test.cs
+++ TerranEditor/SandboxProject/Assets/Scripts/Test.cs
@@ -1,33 +1,13 @@
-﻿using System;
-using Terran;
+﻿using Terran;
 
 namespace ScriptAssembly
 {
-#nullable enable
     internal class Test : Scriptable
     {
-        //public string[] Test1 = new string[] { "cum", "cum2", "cum3" };
-        //public bool TestBool = false;
-        //      public byte TestB = 10;
-        //      public sbyte TestSB = -10;
-        //public ushort TestUS = 10;
-        //      public short TestS = -10;
-        //      public uint TestUI = 10;
-        //      public int TestI = -10;
-        //      public ulong TestUL = 10;
-        //      public long TestL = -10;
-        //      public float TestF = 10.10f;
-        //      public double TestD = -100.0321d;
-        //      public char TestC = 'c';
-        //      public Vector2 TestVec2 = new Vector2(1.0f, 20.0f);
-        //      public Vector3 TestVec3 = new Vector3(10.0312f, 123.0f, 2132f);
-        //      public Color TestCol = new Color(1.0f, 0.5f, 0.0f, 1.0f);
-        //      public string TestStr = "This is a test string! Will it show up?";
-        //      public Entity TestEntity;
-
-        public bool IsGrounded = false;
+        public Entity Tomaaaaa;
 
         protected override void Init()
+<<<<<<< HEAD
 		{
         }
 
@@ -36,9 +16,13 @@ namespace ScriptAssembly
         }
 
         protected override void PhysicsUpdate()
+=======
+>>>>>>> 33ef74f (added mouse scene)
         {
-            Log.Trace("physics update");
+            Test2 test2 = Tomaaaaa.GetComponent<Test2>();
+            Log.Trace(test2.Test);
         }
+<<<<<<< HEAD
 
         protected override void OnCollisionBegin(Entity entity)
         {
@@ -49,6 +33,7 @@ namespace ScriptAssembly
         {
             Log.Trace(entity.Name);
         }
+=======
+>>>>>>> 33ef74f (added mouse scene)
     }
-#nullable disable
 }
