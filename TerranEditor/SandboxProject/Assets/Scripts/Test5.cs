c98387df7d8bcedab5cc5aad89e89ad304ced167
Jordan-Dimitrov <dani27game@gmail.com>
2024-04-11 08:49:47 +0300

    tomaaaa

---
+++ TerranEditor/SandboxProject/Assets/Scripts/Test5.cs
@@ -0,0 +1,21 @@
+﻿using Terran;
+
+namespace ScriptAssembly.Assets.Scripts
+{
+    internal class Test5 : Scriptable
+    {
+        protected override void Init()
+        {
+            base.Init();
+        }
+
+        protected override void Update(float deltaTime)
+        {
+            base.Update(deltaTime);
+            if (Input.IsMouseButtonDown(MouseButton.LeftButton))
+            {
+
+            }
+        }
+    }
+}
