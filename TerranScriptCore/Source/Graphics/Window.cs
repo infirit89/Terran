833c21de06011983aa26a8e6f3c88fae5d3d37d4
infirit89 <georgedautov808@gmail.com>
2024-04-12 10:32:58 +0300

    updated gitignore

---
+++ TerranScriptCore/Source/Graphics/Window.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terran;
+
+namespace TerranScriptCore.Source.Graphics
+{
+    public class Window
+    {
+        public static float Width 
+        {
+            get 
+            {
+                unsafe 
+                {
+                    return Internal.Window_GetWidthICall();
+                }
+            }
+        }
+
+        public static float Height 
+        {
+            get
+            {
+                unsafe 
+                {
+                    return Internal.Window_GetHeightICall();
+                }
+            }
+        }
+
+        public static bool VSync 
+        {
+            get
+            {
+                unsafe 
+                {
+                    return Internal.Window_IsVSyncICall();
+                }
+            }
+        }
+
+        public static Vector2 ContentScale 
+        {
+            get 
+            {
+                unsafe 
+                {
+                    return Internal.Window_GetContentScaleICall();
+                }
+            }
+        }
+    }
+}
