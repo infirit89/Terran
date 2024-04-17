457a9c88a9d3fdacf49d26838cda4d631cceec80
infirit89 <georgedautov808@gmail.com>
2024-04-17 10:24:17 +0300

    added window internal calls

--- TerranEditor/SandboxProject/Assets/Scripts/Test.cs
+++ TerranEditor/SandboxProject/Assets/Scripts/Test.cs
@@ -1,4 +1,5 @@
 ﻿using Terran;
+using TerranScriptCore.Source.Graphics;
 
 namespace ScriptAssembly
 {
@@ -11,33 +12,12 @@ namespace ScriptAssembly
 
         protected override void Init()
         {
-            Log.Trace(TestI);
         }
 
         protected override void Update(float deltaTime)
         {
-            //if (Input.IsKeyDown(KeyCode.A))
-            //    Log.Warn("A is down");
-
-            //if (Input.IsKeyPressed(KeyCode.S))
-            //    Log.Warn("S is pressed");
-
-            //if (Input.IsKeyReleased(KeyCode.D))
-            //    Log.Warn("D is pressed");
-
-            //if (Input.IsMouseButtonDown(MouseButton.LeftButton))
-            //    Log.Warn("Left button down");
-
-            //if (Input.IsMouseButtonPressed(MouseButton.MiddleButton)) 
-            //{
-            //    Log.Warn("Middle button pressed");
-            //    Log.Warn(Input.GetMousePosition());
-            //}
-
-            //if (Input.IsMouseButtonReleased(MouseButton.RightButton))
-            //    Log.Warn("Right button released");
-
-            Log.Warn(Input.GetMousePosition());
+            Log.Warn(Window.Width);
+            Log.Warn(Window.Height);
         }
     }
 }
