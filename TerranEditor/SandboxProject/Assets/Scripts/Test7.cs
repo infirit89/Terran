db1e82b38c85a4c870b0e66d55983112646188d6
Jordan-Dimitrov <dani27game@gmail.com>
2024-04-17 13:05:40 +0300

    added entity test scene

--- TerranEditor/SandboxProject/Assets/Scripts/Test7.cs
+++ TerranEditor/SandboxProject/Assets/Scripts/Test7.cs
@@ -1,10 +1,12 @@
-﻿using Terran;
+﻿using System;
+using Terran;
 using TerranScriptCore.Source.Graphics;
 
 namespace ScriptAssembly
 {
     internal class Test7 : Scriptable
     {
+        public float MouseSensitivity = 0.01f;
         protected override void Init()
         {
             base.Init();
@@ -12,7 +14,11 @@ namespace ScriptAssembly
 
         protected override void Update(float deltaTime)
         {
-            var input = Input.GetMousePosition() + new Vector2(Window.Width / 2, Window.Height / 2);
+            var input = Input.GetMousePosition() + new Vector2(Window.Width / 2, Window.Height / 2) * MouseSensitivity;
+            Log.Trace(input.X);
+            input.X = Math.Clamp(input.X, -Window.Width / 2, Window.Width / 2);
+            Log.Trace(input.X);
+            input.Y = Math.Clamp(input.Y, -Window.Height / 2, Window.Height / 2);
 
             Entity.Transform.Position = new Vector3(input.X, input.Y, 1);
 
