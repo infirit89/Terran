642b489b0b93a9b6fd9e7e08e18d71e22d1ff250
Jordan-Dimitrov <dani27game@gmail.com>
2024-04-18 10:04:31 +0300

    minor fixes

---
+++ TerranEditor/SandboxProject/Assets/Scripts/Test9.cs
@@ -0,0 +1,17 @@
+﻿using Terran;
+
+namespace ScriptAssembly
+{
+    internal class Test9 : Scriptable
+    {
+        protected override void Init()
+        {
+            base.Init();
+        }
+
+        protected override void Update(float deltaTime)
+        {
+            base.Update(deltaTime);
+        }
+    }
+}
