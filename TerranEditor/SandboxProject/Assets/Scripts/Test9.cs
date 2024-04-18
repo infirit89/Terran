52ae3d29e0b1da622037d5196f2c857400b9fc60
Jordan-Dimitrov <dani27game@gmail.com>
2024-04-18 11:13:43 +0300

    added physics test scene

--- TerranEditor/SandboxProject/Assets/Scripts/Test9.cs
+++ TerranEditor/SandboxProject/Assets/Scripts/Test9.cs
@@ -1,17 +1,30 @@
-﻿using Terran;
+﻿using System;
+using Terran;
 
 namespace ScriptAssembly
 {
     internal class Test9 : Scriptable
     {
+        private Random _Random = new Random();
         protected override void Init()
         {
-            base.Init();
         }
 
         protected override void Update(float deltaTime)
         {
-            base.Update(deltaTime);
+            var item = Physics2D.RayCast(new Vector2(5, 0), new Vector2(5, 20));
+
+            if (item)
+            {
+                Log.Warn("Hit entity {0}", item.Rigidbody.Entity.Name);
+
+                float r = (float)_Random.Next(0, 255) / 255;
+                float g = (float)_Random.Next(0, 255) / 255;
+                float b = (float)_Random.Next(0, 255) / 255;
+
+                item.Rigidbody.Entity.GetComponent<SpriteRenderer>().Color = new Color(r, g, b);
+
+            }
         }
     }
 }
