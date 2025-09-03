98d7d45e506c37bafcde9a83f444eee0dfc22815
infirit89 <georgedautov808@gmail.com>
2025-09-03 21:58:35 +0300

    chore(build): improve python setup scripts

--- scripts/Setup.py
+++ scripts/Setup.py
@@ -1,19 +1,20 @@
+from shutil import Error
 import sys
 import subprocess
 import os
 
-packages = [ "requests", "numpy", "GitPython" ]
+packages = [ "requests", "numpy" ]
 
 def InstallPackages():
     for package in packages:
-        sys.stdout.write("\rInstalling package: {}\n".format(package))
+        sys.stdout.write("Installing package: {}\r\n".format(package))
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-        sys.stdout.write('\n')
+        sys.stdout.write('\r\n')
 
 def UpdateSubmodules():
-    sys.stdout.write("\rUpdating submodules")
+    sys.stdout.write("Updating submodules\r\n")
     subprocess.check_call(["git", "submodule", "update", "--init", "--recursive"])
-    sys.stdout.write('\n')
+    sys.stdout.write('\r\n')
 
 if __name__ == "__main__":
     UpdateSubmodules()
@@ -21,7 +22,9 @@ if __name__ == "__main__":
 
     # TODO: install dotnet8 if not installed 
     InstallPackages()
+
     from SetupPremake import PremakeSetup
     from SetupShaderc import ShadercSetup
-    PremakeSetup.Setup()
-    ShadercSetup.Setup()
+
+    PremakeSetup.setup()
+    ShadercSetup.setup()
--- scripts/SetupShaderc.py
+++ scripts/SetupShaderc.py
@@ -5,45 +5,45 @@ import sys
 
 class ShadercSetup:
 
-    shadercInstallDir = f"{Utils.TerranEngineVendorPath}/shaderc"
-    shadercURL = f"https://storage.googleapis.com/shaderc/artifacts/prod/graphics_shader_compiler/shaderc/windows/continuous_release_2017/438/20230810-123700/install.zip"
-    shadercLicenseURL = f"https://raw.githubusercontent.com/google/shaderc/main/LICENSE"
+    SHADERC_INSTALL_DIRECTORY = f"{Utils.TERRAN_ENGINE_VENDOR_PATH}/shaderc"
+    SHADERC_URL = f"https://storage.googleapis.com/shaderc/artifacts/prod/graphics_shader_compiler/shaderc/windows/continuous_release_2017/438/20230810-123700/install.zip"
+    SHADERC_LICENSE_URL = f"https://raw.githubusercontent.com/google/shaderc/main/LICENSE"
 
     @classmethod
-    def Setup(self):
-        if not os.path.exists(self.shadercInstallDir):
-            self.InstallShaderc()
+    def setup(cls):
+        if not os.path.exists(cls.SHADERC_INSTALL_DIRECTORY):
+            cls.__install_shaderc()
         else:
             print("Shaderc already installed!")
 
     @classmethod
-    def InstallShaderc(self):
+    def __install_shaderc(cls):
 
-        os.makedirs(self.shadercInstallDir)
-        shadercZipPath = f"{self.shadercInstallDir}/shaderc.zip"
-        Utils.DownloadFile(self.shadercURL, shadercZipPath)
-        Utils.UnzipFile(shadercZipPath)
-        self.MoveFromTempDirectory()
+        os.makedirs(cls.SHADERC_INSTALL_DIRECTORY)
+        shaderc_zip_path = f"{cls.SHADERC_INSTALL_DIRECTORY}/shaderc.zip"
+        Utils.download_file(cls.SHADERC_URL, shaderc_zip_path)
+        Utils.unzip_file(shaderc_zip_path)
+        cls.__move_from_temporary_directory()
         
         print("Shaderc has been downloaded!")
 
-        shadercLicensePath = f"{self.shadercInstallDir}/LICENSE.txt"
-        Utils.DownloadFile(self.shadercLicenseURL, shadercLicensePath)
+        shaderc_license_path = f"{cls.SHADERC_INSTALL_DIRECTORY}/LICENSE.txt"
+        Utils.download_file(cls.SHADERC_LICENSE_URL, shaderc_license_path)
         print("Shaderc license has been downloaded")
 
     @classmethod
-    def MoveFromTempDirectory(self):
-        shadercTempPath = os.path.abspath(f"{self.shadercInstallDir}/install")
-        totalEntryCount = len(os.listdir(shadercTempPath))
+    def __move_from_temporary_directory(cls):
+        shaderc_temporary_path = os.path.abspath(f"{cls.SHADERC_INSTALL_DIRECTORY}/install")
+        total_entry_count = len(os.listdir(shaderc_temporary_path))
         moved = 0
         
-        for subdir in os.scandir(shadercTempPath):
-            if subdir.is_dir():
-                shutil.move(f"{shadercTempPath}/{subdir.name}", self.shadercInstallDir)
-                sys.stdout.write("\rMoved: {}/{}".format(moved, totalEntryCount))
+        for subdirectory in os.scandir(shaderc_temporary_path):
+            if subdirectory.is_dir():
+                shutil.move(f"{shaderc_temporary_path}/{subdirectory.name}", cls.SHADERC_INSTALL_DIRECTORY)
+                sys.stdout.write("\rMoved: {}/{}".format(moved, total_entry_count))
                 sys.stdout.flush()
                 moved += 1
         sys.stdout.write("\n")
         
-        shutil.rmtree(shadercTempPath)
+        shutil.rmtree(shaderc_temporary_path)
 
