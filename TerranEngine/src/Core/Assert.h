cc96018db53d6dd0afe912ddfb00f5a097673fd4
infirit89 <georgedautov808@gmail.com>
2025-09-04 17:06:27 +0300

    chore(macos): start porting to a non-windows platform
    BROKEN DOES NOT BUILD!

--- TerranEngine/src/Core/Assert.h
+++ TerranEngine/src/Core/Assert.h
@@ -1,9 +1,17 @@
 #pragma once
 
 #include "Log.h"
+#include "PlatformCheck.h"
+
+#if defined(TR_WINDOWS)
+	#define TR_DEBUGBREAK __debugbreak
+#elif defined(TR_MACOSX)
+	#define TR_DEBUGBREAK __builtin_trap
+#endif
+
 
 #if defined(TR_DEBUG)
-	#define TR_ASSERT(condition, ...) if(!(condition)) { TR_CORE_ERROR(TR_LOG_CORE, __VA_ARGS__); __debugbreak(); }
+	#define TR_ASSERT(condition, ...) if(!(condition)) { TR_CORE_ERROR(TR_LOG_CORE, __VA_ARGS__); TR_DEBUGBREAK(); }
 #elif defined(TR_RELEASE)
 	#define TR_ASSERT
 #endif
