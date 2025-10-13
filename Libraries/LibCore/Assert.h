24a1a16eed9ba019791e4cb6ae6771b8c3e12942
infirit89 <georgedautov808@gmail.com>
2025-10-13 20:44:48 +0300

    refactor(Core): change some qol stuff Revert to using TR_DEBUG and TR_RELEASE Fix event compilation errors

--- Libraries/LibCore/Assert.h
+++ Libraries/LibCore/Assert.h
@@ -4,14 +4,17 @@
 #include "PlatformCheck.h"
 
 #if defined(TR_WINDOWS)
-	#define TR_DEBUGBREAK __debugbreak
+#    define TR_DEBUGBREAK __debugbreak
 #elif defined(TR_MACOSX)
-	#define TR_DEBUGBREAK __builtin_trap
+#    define TR_DEBUGBREAK __builtin_trap
 #endif
 
-
-#if defined(TR_CORE_DEBUG)
-	#define TR_ASSERT(condition, ...) if(!(condition)) { TR_CORE_ERROR(TR_LOG_CORE, __VA_ARGS__); TR_DEBUGBREAK(); }
+#if defined(TR_DEBUG)
+#    define TR_ASSERT(condition, ...)                \
+        if (!(condition)) {                          \
+            TR_CORE_ERROR(TR_LOG_CORE, __VA_ARGS__); \
+            TR_DEBUGBREAK();                         \
+        }
 #elif defined(TR_RELEASE)
-	#define TR_ASSERT
+#    define TR_ASSERT
 #endif
