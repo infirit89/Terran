349e0c500ab3c4a4bf2de8eaf31c183240c9cfd1
infirit89 <georgedautov808@gmail.com>
2025-10-18 12:04:58 +0300

    chore(Main): add application startup and shutdown userdefined hooks

--- Libraries/LibMain/Application.cpp
+++ Libraries/LibMain/Application.cpp
@@ -6,6 +6,7 @@
 #include "Application.h"
 #include "LibCore/Assert.h"
 #include "LibCore/Time.h"
+#include <LibCore/Result.h>
 
 namespace Terran {
 namespace Main {
@@ -39,23 +40,28 @@ void Application::dispatch_event(Terran::Core::Event& event)
     // TODO: Implement event dispatching system
 }
 
-void Application::run()
+Terran::Core::Result<void> Application::run()
 {
     TR_ASSERT(!m_Instance->m_Running, "The application is already running");
-    
+
+    for (auto const& hook : m_startupHooks) {
+        auto result = hook();
+        if (!result.is_ok())
+            return result;
+    }
     m_Running = true;
     float lastFrameTime = 0.0f;
 
     while (m_Running) {
         // TODO: Re-enable profiling when system is stabilized
         // TR_PROFILE_FRAME("MainThread");
-        
+
         // TODO: Implement proper frame timing
         // NOTE: Consider changing frametime to be a double for better precision
         // float frameTime = glfwGetTime();
         // Terran::Core::Time time(frameTime - lastFrameTime);
         // lastFrameTime = frameTime;
-        
+
         Terran::Core::Time time(0.0f);
 
         if (!m_Minimized) {
@@ -76,7 +82,16 @@ void Application::run()
             // m_ImGuiLayer->EndFrame();
         }
     }
+
+    for (auto const& hook : m_shutdownHooks) {
+        auto result = hook();
+        if (!result.is_ok())
+            return result;
+    }
+
+    return {};
 }
+
 
 // void Application::DispatchEvent(Terran::Core::Event& event)
 // {
