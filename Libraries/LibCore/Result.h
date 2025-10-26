e97ca10d2f450359a0bc465c70e5d28aff9a9028
infirit89 <georgedautov808@gmail.com>
2025-10-26 14:18:45 +0200

    chore(Core): improve Result type Create Core::Bad type for easily returning errors

--- Libraries/LibCore/Result.h
+++ Libraries/LibCore/Result.h
@@ -1,21 +1,24 @@
 #pragma once
 
 #include "LibCore/Assert.h"
+#include <LibCore/Macros.h>
+#include <type_traits>
+#include <utility>
 namespace Terran {
 namespace Core {
 
 // NOTE: maybe move into some ErrorMessages file?
 #ifdef TR_DEBUG
-#define TR_RESULT_VALUE_ACCESS_WHEN_ERROR_STATE_MESSAGE "Cannot access the value of a result when in Error variant!"
-#define TR_RESULT_ERROR_ACCESS_WHEN_VALUE_STATE_MESSAGE "Cannot access the error of a result when in Ok variant!"
+#    define TR_RESULT_VALUE_ACCESS_WHEN_ERROR_STATE_MESSAGE "Cannot access the value of a result when in Error variant!"
+#    define TR_RESULT_ERROR_ACCESS_WHEN_VALUE_STATE_MESSAGE "Cannot access the error of a result when in Ok variant!"
 #else
-#define TR_RESULT_VALUE_ACCESS_WHEN_ERROR_STATE_MESSAGE
-#define TR_RESULT_ERROR_ACCESS_WHEN_VALUE_STATE_MESSAGE
+#    define TR_RESULT_VALUE_ACCESS_WHEN_ERROR_STATE_MESSAGE
+#    define TR_RESULT_ERROR_ACCESS_WHEN_VALUE_STATE_MESSAGE
 #endif
 
-struct Empty {};
-struct EmptyError {};
-// NOTE: new thing learned, if the base definition of result 
+struct Empty { };
+struct EmptyError { };
+// NOTE: new thing learned, if the base definition of result
 // is temlate<typename T, typename Err = Empty> Result,
 // when result is Result<void>
 // due to the partial specialization it becomes Result<Empty, Empty>
@@ -26,55 +29,73 @@ class Result {
 public:
     using result_type = TValue;
     using error_type = TError;
-    constexpr Result() noexcept = default;
-    constexpr Result(TValue const& data) noexcept
+
+    MAKE_NONCOPYABLE(Result);
+
+    constexpr Result()
+    requires(std::is_default_constructible_v<TValue>)
+    = default;
+
+    template<typename U>
+    constexpr Result(U const& data) noexcept
+    requires(std::is_convertible_v<U, TValue>)
         : m_storage(data)
         , m_isError(false)
     {
     }
 
-    constexpr Result(TError const& error) noexcept
-        : m_storage(error)
-        , m_isError(true)
+    template<typename E>
+    constexpr Result(E const& error) noexcept
+    requires(std::is_convertible_v<E, TError>)
+        : m_isError(true)
     {
+        m_storage.error = error;
     }
+
     constexpr ~Result() noexcept = default;
 
-    constexpr bool is_ok() const {
+    [[nodiscard]] constexpr bool is_ok() const
+    {
         return !m_isError;
     }
 
-    constexpr operator bool() const {
+    constexpr operator bool() const
+    {
         return is_ok();
     }
 
-    constexpr TValue& value() & noexcept {
+    [[nodiscard]] constexpr TValue& value() & noexcept
+    {
         TR_ASSERT(!m_isError, TR_RESULT_VALUE_ACCESS_WHEN_ERROR_STATE_MESSAGE);
         return m_storage.data;
     }
 
-    constexpr const TValue& value() const& noexcept {
+    [[nodiscard]] constexpr TValue const& value() const& noexcept
+    {
         TR_ASSERT(!m_isError, TR_RESULT_VALUE_ACCESS_WHEN_ERROR_STATE_MESSAGE);
         return m_storage.data;
     }
 
-    constexpr TError& error() & noexcept {
+    [[nodiscard]] constexpr TError& error() & noexcept
+    {
         TR_ASSERT(m_isError, TR_RESULT_ERROR_ACCESS_WHEN_VALUE_STATE_MESSAGE);
         return m_storage.error;
     }
 
-    constexpr const TError& error() const& noexcept {
+    [[nodiscard]] constexpr TError const& error() const& noexcept
+    {
         TR_ASSERT(m_isError, TR_RESULT_ERROR_ACCESS_WHEN_VALUE_STATE_MESSAGE);
         return m_storage.error;
     }
 
 private:
-    union {
+    union storage {
         TValue data;
         TError error;
+        ~storage() { }
     } m_storage = {};
 
-    bool m_isError = false;
+    bool m_isError;
 };
 
 template<typename TError>
@@ -82,6 +103,26 @@ class Result<void, TError> : public Result<Empty, TError> {
 public:
     using result_type = void;
     using Result<Empty, TError>::Result;
+};
+
+template<typename TError = EmptyError>
+class Bad {
+public:
+    Bad()
+    requires(std::is_default_constructible_v<TError>)
+    = default;
+    Bad(TError const& error)
+        : m_error(error)
+    {
+    }
+
+    operator Result<void, TError>() const { return { m_error }; }
+
+    template<typename TValue>
+    operator Result<TValue, TError>() const { return { m_error }; }
+
+private:
+    TError m_error;
 };
 
 }
