25432e8434d9233bb9d45be7214a38153d33e302
infirit89 <georgedautov808@gmail.com>
2025-11-05 23:59:55 +0200

    chore(Core): update layer stack

--- Libraries/LibCore/LayerStack.h
+++ Libraries/LibCore/LayerStack.h
@@ -2,8 +2,12 @@
 
 #include "Layer.h"
 #include "Macros.h"
+#include "Unique.h"
+#include "Result.h"
 
+#include <cstddef>
 #include <vector>
+#include <utility>
 
 namespace Terran {
 
@@ -11,24 +15,77 @@ namespace Core {
 
 class LayerStack final {
 public:
+    using layer_container = std::vector<Unique<Layer>>;
     // i dont currently see a reason why layerstack should be copyable or moveable
     MAKE_NONCOPYABLE(LayerStack);
     MAKE_NONMOVEABLE(LayerStack);
     LayerStack() = default;
-    ~LayerStack();
+    ~LayerStack()
+    {
+        clear();
+    }
 
-    void push(Layer* layer);
-    void remove(Layer* layer);
-    void clear();
+    template<typename... Args>
+    Result<void> push(Args... args)
+    {
+        Unique<Layer> layer = Unique<Layer>::create(std::forward<Args>(args)...);
+        auto result = layer->on_attach();
 
-    constexpr std::vector<Layer*> const& layers() const noexcept
+        if(!result.is_ok())
+            return result;
+
+        m_layers.emplace_back(std::move(layer));
+    }
+
+    // TODO: error handling for on dettach
+    void remove_at(size_t index)
+    {
+        auto& layer = m_layers.at(index);
+        layer->on_dettach();
+        m_layers.erase(m_layers.begin() + index);
+    }
+
+    // TODO: error handling for on dettach
+    void clear()
+    {
+        while (!m_layers.empty()) {
+            auto& layer = m_layers.at(m_layers.size() - 1);
+            layer->on_dettach();
+            m_layers.erase(m_layers.end() - 1);
+        }
+    }
+
+    constexpr layer_container const& data() const noexcept
     {
         return m_layers;
     }
 
+    constexpr layer_container::iterator begin() noexcept {
+        return m_layers.begin();
+    }
+
+    constexpr layer_container::const_iterator begin() const noexcept {
+        return m_layers.begin();
+    }
+
+    constexpr layer_container::const_iterator cbegin() const noexcept {
+        return m_layers.cbegin();
+    }
+
+    constexpr layer_container::iterator end() noexcept {
+        return m_layers.end();
+    }
+
+    constexpr layer_container::const_iterator end() const noexcept {
+        return m_layers.end();
+    }
+
+    constexpr layer_container::const_iterator cend() const noexcept {
+        return m_layers.cend();
+    }
+
 private:
-    std::vector<Layer*> m_layers;
-    int m_lastInsertIndex = 0;
+    layer_container m_layers;
 };
 
 }
