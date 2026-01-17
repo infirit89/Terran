cc55ac13094da9f44d5fcab6bb2027ff3875a242
infirit89 <georgedautov808@gmail.com>
2026-01-17 17:58:48 +0200

    chore(Core): add 64 bit fnv1a hashing function

--- Libraries/LibCore/Hash.cpp
+++ Libraries/LibCore/Hash.cpp
@@ -7,12 +7,12 @@
 namespace Terran {
 namespace Core {
 
-uint32_t Hash::FNVHash(std::string const& str)
+uint32_t Hash::fnv1a(std::string const& str)
 {
-    return FNVHash(std::string_view(str));
+    return fnv1a(std::string_view(str));
 }
 
-uint32_t Hash::FNVHash(std::string_view str)
+uint32_t Hash::fnv1a(std::string_view str)
 {
     constexpr uint32_t const FNV32Prime = 16777619;
     constexpr uint32_t const FNV32Offset = 2166136261;
@@ -22,6 +22,20 @@ uint32_t Hash::FNVHash(std::string_view str)
     for (char c : str) {
         hash = hash ^ c;
         hash = hash * FNV32Prime;
+    }
+
+    return hash;
+}
+
+uint64_t Hash::fnv1a_64(std::string_view str) {
+    constexpr uint64_t const FNV64Prime = 0x00000100000001b3;
+    constexpr uint64_t const FNV64Offset = 0xcbf29ce484222325;
+
+    uint64_t hash = FNV64Offset;
+
+    for (char c : str) {
+        hash = hash ^ c;
+        hash = hash * FNV64Prime;
     }
 
     return hash;
