7f703d43c7b5b5effdd0da5c71fe2410b3fd4be0
infirit89 <georgedautov808@gmail.com>
2026-02-15 23:33:13 +0200

    chore(Core): fix sonarcloud issues

--- Libraries/LibCore/SerializerExtensions.h
+++ Libraries/LibCore/SerializerExtensions.h
@@ -8,11 +8,38 @@
 
 namespace Terran::Core {
 
-YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec2 const& v);
-YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec3 const& v);
-YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec4 const& v);
-YAML::Emitter& operator<<(YAML::Emitter& out, Terran::Core::UUID const& v);
-YAML::Emitter& operator<<(YAML::Emitter& out, std::byte v);
+struct CustomConversionOperators {
+    friend YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec2 const& v)
+    {
+        out << YAML::Flow;
+        out << YAML::BeginSeq << v.x << v.y << YAML::EndSeq;
+        return out;
+    }
+
+    friend YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec3 const& v)
+    {
+        out << YAML::Flow;
+        out << YAML::BeginSeq << v.x << v.y << v.z << YAML::EndSeq;
+        return out;
+    }
+
+    friend YAML::Emitter& operator<<(YAML::Emitter& out, glm::vec4 const& v)
+    {
+        out << YAML::Flow;
+        out << YAML::BeginSeq << v.x << v.y << v.z << v.w << YAML::EndSeq;
+        return out;
+    }
+    friend YAML::Emitter& operator<<(YAML::Emitter& out, Terran::Core::UUID const& v)
+    {
+        out << std::to_string(v);
+        return out;
+    }
+    friend YAML::Emitter& operator<<(YAML::Emitter& out, std::byte v)
+    {
+        out << static_cast<int>(v);
+        return out;
+    }
+};
 
 }
 
